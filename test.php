3d065ff03ad843686b71f94eb4901a107c2e0c4e
Justin <suddenlysquirrels@yahoo.com>
2015-05-30 06:15:30 -0700

    d  with '#' will be ignored, and an empty message aborts the commit.

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-	$data = "hate hate hate hate hate hate hate, 2\n hate hate hate hate hate hate hate, 5\n hate hate hate hate hate hate hate, 0";
+	$data = "hate 4;: $ hate hate hate hate ~`ha!@#$%^&*(&^%$#$%^&*&^%$TFGG:\"?te hate, 2\n hate hate hate hate hate hate hate, 5\n hate hate hate hate hate hate hate, 0\n";
 	$result = exec('python example.py ' . escapeshellarg($data)); //. escapeshellarg(json_encode($data))
 	$resultData = json_decode($result, true);
 	print 'Average: ' . $resultData[0] . '<br>';
